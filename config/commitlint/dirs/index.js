e1a66f6bcd85da3da7b7f5dea2413f657b33c631
akky-xxxx <114.s.aki@gmail.com>
2024-06-02 15:24:42 +0900

    chore(root): add error handling into config/commitlint/dirs

--- config/commitlint/dirs/index.js
+++ config/commitlint/dirs/index.js
@@ -1,10 +1,16 @@
 const fs = require("fs")
 
 const getDirs = (packagesDir) => {
-  const dir2path = (dir) => `${packagesDir}/${dir}`
-  const isDir = (path) => fs.statSync(path).isDirectory()
-  const path2dir = (path) => path.replace(`${packagesDir}/`, "")
-  return fs.readdirSync(packagesDir).map(dir2path).filter(isDir).map(path2dir)
+  try {
+    const dir2path = (dir) => `${packagesDir}/${dir}`
+    const isDir = (path) => fs.statSync(path).isDirectory()
+    const path2dir = (path) => path.replace(`${packagesDir}/`, "")
+    return fs.readdirSync(packagesDir).map(dir2path).filter(isDir).map(path2dir)
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+    throw error
+  }
 }
 
 exports.dirs = [
