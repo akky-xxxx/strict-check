826a6b03b8f65d98b9a0cdbba47871b8a154044c
akky-xxxx <114.s.aki@gmail.com>
2024-06-02 15:37:31 +0900

    chore(eslint-plugin-js): store config files

---
+++ packages/eslint-plugin-js/eslint.config.mjs
@@ -0,0 +1,31 @@
+import { BASE_RECORDS } from "@strict/monorepo/config/eslint/RECORDS/BASE_RECORDS/index.mjs"
+import { CONFIGS_RECORD } from "@strict/monorepo/config/eslint/RECORDS/CONFIGS_RECORD/index.mjs"
+import { ESLINT_CONFIG_RECORD } from "@strict/monorepo/config/eslint/RECORDS/ESLINT_CONFIG_RECORD/index.mjs"
+import { SCRIPTS_RECORD } from "@strict/monorepo/config/eslint/RECORDS/SCRIPTS_RECORD/index.mjs"
+import { IGNORE_RECORD } from "@strict/monorepo/config/eslint/records/IGNORE_RECORD/index.mjs"
+import { TEST_RECORD } from "@strict/monorepo/config/eslint/records/TEST_RECORD/index.mjs"
+import { TYPESCRIPT_RECORDS } from "@strict/monorepo/config/eslint/records/TYPESCRIPT_RECORDS/index.mjs"
+
+const CONFIG = [
+  IGNORE_RECORD,
+  {
+    ignores: ["src/libs/**"],
+  },
+  ...BASE_RECORDS,
+  ...TYPESCRIPT_RECORDS,
+  SCRIPTS_RECORD,
+  CONFIGS_RECORD,
+  ESLINT_CONFIG_RECORD,
+  TEST_RECORD,
+  {
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".ts"],
+        },
+      },
+    },
+  },
+]
+
+export default CONFIG
