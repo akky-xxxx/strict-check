d26b11bcb90d4dc33f6fee45e70350206b9471b5
akky-xxxx <114.s.aki@gmail.com>
2024-06-02 15:43:27 +0900

    chore(root): implement copy-file, copy-config

---
+++ scripts/copy-shared/index.js
@@ -0,0 +1,78 @@
+const fs = require("fs")
+const path = require("path")
+
+const { DIRECTORIES } = require("../shared/constants/DIRECTORIES")
+const { copyFile } = require("../shared/utilities/copyFile")
+const { getFilePath } = require("../shared/utilities/getFilePath")
+const { getFilePaths } = require("../shared/utilities/getFilePaths")
+const { getFsMethod } = require("../shared/utilities/getFsMethod")
+
+const THIS_DIRECTORY_NAME = __dirname
+
+const FIRST_CHARACTER = 0
+const ROOT = THIS_DIRECTORY_NAME.slice(
+  FIRST_CHARACTER,
+  THIS_DIRECTORY_NAME.lastIndexOf(DIRECTORIES.PROJECT_NAME) + DIRECTORIES.PROJECT_NAME.length,
+)
+
+const SHARED_DIRECTORY =
+  path.join(ROOT, DIRECTORIES.WS_MODULES)
+const PACKAGE_DIRECTORY =
+  path.join(ROOT, DIRECTORIES.WS_PACKAGES)
+
+const isEslintConfigDirectory = (directory) =>
+  directory.includes(DIRECTORIES.ESLINT_CONFIG)
+const isEslintPluginDirectory = (directory) =>
+  directory.includes(DIRECTORIES.ESLINT_PLUGIN)
+const isSharedDirectory = (directory) =>
+  directory.startsWith(DIRECTORIES.SHARED_PREFIX)
+
+const convertToLibsPath = (destName) =>
+  (directory) =>
+    path.join(
+      PACKAGE_DIRECTORY,
+      directory,
+      DIRECTORIES.SRC,
+      DIRECTORIES.LIBS,
+      destName,
+    )
+const convertToSrcPath =
+  (directory) =>
+    path.join(SHARED_DIRECTORY, directory, DIRECTORIES.SRC)
+
+const eslintConfigPaths = fs.readdirSync(PACKAGE_DIRECTORY)
+  .filter(isEslintConfigDirectory)
+  .map(convertToLibsPath(DIRECTORIES.DEST_CONFIG))
+const eslintPluginPaths = fs.readdirSync(PACKAGE_DIRECTORY)
+  .filter(isEslintPluginDirectory)
+  .map(convertToLibsPath(DIRECTORIES.DEST_PLUGIN))
+const sharedPaths = fs.readdirSync(SHARED_DIRECTORY)
+  .filter(isSharedDirectory)
+  .map(convertToSrcPath)
+
+sharedPaths.forEach((sharedPath) => {
+  if (sharedPath.includes(DIRECTORIES.ESLINT_CONFIG)) {
+    eslintConfigPaths.forEach((configPath) => {
+      const existsTarget = fs.existsSync(configPath)
+      const fsMethod = getFsMethod(existsTarget)
+      fs[fsMethod](configPath, { recursive: true })
+
+      getFilePaths(sharedPath)
+        .map(getFilePath(DIRECTORIES.SRC))
+        .forEach(copyFile(sharedPath, configPath))
+    })
+  }
+  if (sharedPath.includes(DIRECTORIES.ESLINT_PLUGIN)) {
+    eslintPluginPaths.forEach((pluginPath) => {
+      const existsTarget = fs.existsSync(pluginPath)
+      const fsMethod = getFsMethod(existsTarget)
+      fs[fsMethod](pluginPath, { recursive: true })
+
+      getFilePaths(sharedPath)
+        .map(getFilePath(DIRECTORIES.SRC))
+        .forEach(copyFile(sharedPath, pluginPath))
+    })
+  }
+})
+
+console.log("Finished the shared files copy.")
---
+++ scripts/shared/constants/DIRECTORIES/index.js
@@ -0,0 +1,21 @@
+const DIRECTORIES = {
+  PROJECT_NAME: "strict",
+
+  WS_MODULES: "modules",
+  WS_PACKAGES: "packages",
+
+  ESLINT_CONFIG: "eslint-config",
+  ESLINT_PLUGIN: "eslint-plugin",
+
+  DEST_CONFIG: "shared-for-config",
+  DEST_PLUGIN: "shared-for-plugin",
+
+  CONFIG: "config",
+  LIBS: "libs",
+  SHARED_PREFIX: "shared",
+  SRC: "src",
+}
+
+module.exports = {
+  DIRECTORIES,
+}
