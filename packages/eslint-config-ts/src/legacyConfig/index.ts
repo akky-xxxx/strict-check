1094166b2088b2753fa7b6777b5e880ba42b2b73
akky-xxxx <114.s.aki@gmail.com>
2024-06-09 21:10:43 +0900

    feat(eslint-config-ts): add rules of eslint-config-sc-js

--- packages/eslint-config-ts/src/legacyConfig/index.ts
+++ packages/eslint-config-ts/src/legacyConfig/index.ts
@@ -1,10 +1,15 @@
+import eslintConfigSCJs from "eslint-config-sc-js"
+
 import { baseRules } from "../shared/config/rules/baseRules"
 import { settings } from "../shared/config/settings"
 
 import type { EslintLegacyConfig } from "../libs/shared-for-config/types/EslintLegacyConfig"
 
+const ZERO = 0
+
 export const legacyConfig = {
   extends: [
+    "plugin:@stylistic/recommended-extends",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:unicorn/recommended",
@@ -12,6 +17,7 @@ export const legacyConfig = {
   ],
   plugins: ["@typescript-eslint", "unicorn"],
   rules: {
+    ...eslintConfigSCJs.configs.customRecords[ZERO].rules,
     ...baseRules,
   },
   settings: {
