e984945d9b607706f394206362dd467625dd4df5
akky-xxxx <114.s.aki@gmail.com>
2024-06-09 18:43:16 +0900

    feat(eslint-config-js): specify all extensions related javascript to files

--- packages/eslint-config-js/src/shared/config/records/baseRecords1/index.ts
+++ packages/eslint-config-js/src/shared/config/records/baseRecords1/index.ts
@@ -6,6 +6,12 @@ import { getCompatExtends } from "../../../../libs/shared-for-config/utilities/g
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
 export const baseRecords1 = [
+  {
+    files: [
+      "**/*.{js,jsx,mjs,cjs}",
+      "**/*.{ts,tsx,mts,cts}",
+    ],
+  },
   stylistic.configs["recommended-flat"],
   eslint.configs.recommended,
   ...getCompatExtends("plugin:unicorn/recommended"),
