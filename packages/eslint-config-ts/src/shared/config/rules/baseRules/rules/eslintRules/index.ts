b216886e5267daffa9571a79b7c9dfe3a8ba1b9b
akky-xxxx <114.s.aki@gmail.com>
2024-06-15 01:47:20 +0900

    feat(eslint-config-ts): adjust eslint rules for ts

--- packages/eslint-config-ts/src/shared/config/rules/baseRules/rules/eslintRules/index.ts
+++ packages/eslint-config-ts/src/shared/config/rules/baseRules/rules/eslintRules/index.ts
@@ -4,21 +4,8 @@ import type { EslintRules } from "../../../../../../libs/shared-for-config/types
 
 export const eslintRules = {
   /* eslint-disable @typescript-eslint/no-magic-numbers */
-  "complexity": [SEVERITY.ERROR, 5],
-  "max-depth": [SEVERITY.ERROR, 3],
-  "max-len": SEVERITY.OFF, // check by stylistic
-  "max-lines": [SEVERITY.ERROR, { max: 100 }],
-  "max-nested-callbacks": [SEVERITY.ERROR, 3],
-  "max-statements": [SEVERITY.ERROR, 10],
-  "multiline-comment-style": SEVERITY.OFF,
-  "no-magic-numbers": SEVERITY.WARN, // TODO: off にして typescript-eslint に移乗する
-  "no-multi-spaces": SEVERITY.OFF, // check by stylistic
-  "no-undefined": SEVERITY.WARN, // TODO: off にする
-  "no-unused-vars": SEVERITY.OFF, // check by tsc
-  "no-use-before-define": SEVERITY.OFF, // check by typescript-eslint
-  "object-curly-newline": SEVERITY.OFF, // check by stylistic
-  "quotes": SEVERITY.OFF, // check by stylistic
-  "semi": SEVERITY.OFF, // check by stylistic
-  "sort-keys": [SEVERITY.WARN, "asc", { allowLineSeparatedGroups: true }],
+  "no-magic-numbers": SEVERITY.OFF,
+  "no-unused-vars": SEVERITY.OFF,
+  "no-use-before-define": SEVERITY.OFF,
   /* eslint-enable @typescript-eslint/no-magic-numbers */
 } as const satisfies EslintRules
