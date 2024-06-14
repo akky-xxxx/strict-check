538a7ae5570a04b5fd2e2dee89260ac902355718
akky--xxxx <114.s.aki@gmail.com>
2024-06-14 20:36:38 +0900

    chore(dr-eslint-config-sc-storybook): store config files for legacy config

---
+++ dry-run/dr-eslint-config-sc-storybook/tests/.eslintrc.jsx.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["sc-jest/legacy"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+}
---
+++ dry-run/dr-eslint-config-sc-storybook/tests/legacy/tsx-test.stories.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable semi */
+const Component = () => 1
+
+const meta = {
+  component: Component,
+}
+
+export default meta
+
+export const Story1 = {}
+
+export const Story2 = {}
