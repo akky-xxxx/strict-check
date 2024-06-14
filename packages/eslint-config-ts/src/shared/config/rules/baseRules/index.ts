262c3c1a9afe9fd3ba86c1ee1d450bec6a5b34af
akky-xxxx <114.s.aki@gmail.com>
2024-06-15 01:46:59 +0900

    refactor(eslint-config-ts): remove duplicated rules

--- packages/eslint-config-ts/src/shared/config/rules/baseRules/index.ts
+++ packages/eslint-config-ts/src/shared/config/rules/baseRules/index.ts
@@ -1,13 +1,9 @@
 import { eslintRules } from "./rules/eslintRules"
-import { importRules } from "./rules/importRules"
 import { typescriptEslintRules } from "./rules/typescriptEslintRules"
-import { unicornRules } from "./rules/unicornRules"
 
 import type { EslintRules } from "../../../../libs/shared-for-config/types/EslintRules"
 
 export const baseRules = {
   ...eslintRules,
-  ...importRules,
   ...typescriptEslintRules,
-  ...unicornRules,
 } as const satisfies EslintRules
