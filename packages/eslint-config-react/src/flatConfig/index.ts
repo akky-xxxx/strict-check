1d815b9cabcdf0cf59e9c5d90d261a7c8ce25320
akky-xxxx <114.s.aki@gmail.com>
2024-06-15 16:43:31 +0900

    feat(eslint-config-react): use baseRecord1

--- packages/eslint-config-react/src/flatConfig/index.ts
+++ packages/eslint-config-react/src/flatConfig/index.ts
@@ -1,3 +1,4 @@
+import { baseRecords1 } from "../shared/config/records/baseRecords1"
 import { baseRecords2 } from "../shared/config/records/baseRecords2"
 import { customRecords } from "../shared/config/records/customRecords"
 import { customRecordsWithTypescript } from "../shared/config/records/customRecordsWithTypescript"
@@ -6,6 +7,7 @@ import { resetRecordsForStylistic } from "../shared/config/records/resetRecordsF
 import type { EslintFlatConfig } from "../libs/shared-for-config/types/EslintFlatConfig"
 
 export const flatConfig = [
+  ...baseRecords1,
   ...baseRecords2,
   ...customRecords,
   ...customRecordsWithTypescript,
--- packages/eslint-config-react/src/index.ts
+++ packages/eslint-config-react/src/index.ts
@@ -1,5 +1,6 @@
 import { flatConfig } from "./flatConfig"
 import { legacyConfig } from "./legacyConfig"
+import { baseRecords1 } from "./shared/config/records/baseRecords1"
 import { baseRecords2 } from "./shared/config/records/baseRecords2"
 import { customRecords } from "./shared/config/records/customRecords"
 import { customRecordsWithTypescript } from "./shared/config/records/customRecordsWithTypescript"
@@ -16,6 +17,7 @@ const plugin = {
     legacy: legacyConfig,
     recommended: flatConfig,
 
+    baseRecords1,
     baseRecords2,
 
     customRecords,
