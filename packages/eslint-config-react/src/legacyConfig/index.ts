cdb1873bbb612c104dba2ddccbe8d5510368111e
akky-xxxx <114.s.aki@gmail.com>
2024-06-15 16:46:10 +0900

    feat(eslint-config-react): extend js rules for legacy config

--- packages/eslint-config-react/src/legacyConfig/index.ts
+++ packages/eslint-config-react/src/legacyConfig/index.ts
@@ -1,10 +1,17 @@
+import eslintConfigSCJs from "eslint-config-sc-js"
+
 import { baseRules } from "../shared/config/rules/baseRules"
 import { typescriptRules } from "../shared/config/rules/typescriptRules"
 
 import type { EslintLegacyConfig } from "../libs/shared-for-config/types/EslintLegacyConfig"
 
+const ZERO = 0
+
 export const legacyConfig = {
   extends: [
+    "plugin:@stylistic/recommended-extends",
+    "eslint:recommended",
+    "plugin:unicorn/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react/recommended",
     "airbnb",
@@ -12,7 +19,9 @@ export const legacyConfig = {
   ],
   plugins: ["react", "react-hooks"],
   rules: {
+    ...eslintConfigSCJs.configs.customRecords[ZERO].rules,
     ...baseRules,
+    ...eslintConfigSCJs.configs.resetRecordsForStylistic[ZERO].rules,
   },
 
   overrides: [
