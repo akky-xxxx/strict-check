caa73b8d230e56ca9b86af7a55a18a9db0cd598a
akky-xxxx <114.s.aki@gmail.com>
2024-06-15 16:56:55 +0900

    test(dr-eslint-config-react): update tests

--- dry-run/dr-eslint-config-sc-react/tests/flat/jsx-test.jsx
+++ dry-run/dr-eslint-config-sc-react/tests/flat/jsx-test.jsx
@@ -1,13 +1,8 @@
 /* eslint-disable no-console, react/prop-types */
-/* eslint-disable quotes, semi, object-curly-newline */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import React, { Fragment } from "react"
 
-/**
- * @remarks
- * for checking apply if eslint-plugin-react
- */
-
+// eslint-disable-next-line complexity
 export const ReactTest1 = (props) => {
   const { bool, count, empty, title } = props
   const state = React.useState("")
@@ -15,6 +10,7 @@ export const ReactTest1 = (props) => {
   console.log(state)
 
   const onClick = () => {
+    // eslint-disable-next-line unicorn/consistent-function-scoping
     const handleClick = () => {
       //
     }
@@ -73,12 +69,14 @@ export const ReactTest3 = (props) => {
   return null
 }
 
+/* eslint-disable sort-keys */
 const reactTest3Props = {
   isStart: true,
   hasData: true,
   existData: true,
   enableSubmit: true,
 }
+/* eslint-enable sort-keys */
 
 export const ReactTest4 = () => (
   // eslint-disable-next-line react/jsx-props-no-spreading
--- dry-run/dr-eslint-config-sc-react/tests/flat/tsx-test.tsx
+++ dry-run/dr-eslint-config-sc-react/tests/flat/tsx-test.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-console, import/no-extraneous-dependencies */
-/* eslint-disable quotes, semi, object-curly-newline */
+/* eslint-disable max-lines */
 // @ts-ignore
 import React, { Fragment } from "react"
 
@@ -13,11 +13,7 @@ type ReactTest1Props = {
   title: string
 }
 
-/**
- * @remarks
- * for checking apply if eslint-plugin-react
- */
-
+// eslint-disable-next-line complexity
 export const ReactTest1: FC<ReactTest1Props> = (props) => {
   const { bool, count, empty, title } = props
   const state = React.useState("")
@@ -25,6 +21,7 @@ export const ReactTest1: FC<ReactTest1Props> = (props) => {
   console.log(state)
 
   const onClick: React.EventHandler<unknown> = () => {
+    // eslint-disable-next-line unicorn/consistent-function-scoping
     const handleClick = () => {
       //
     }
@@ -97,11 +94,13 @@ export const ReactTest3 = (props: ReactTest3Props) => {
   return null
 }
 
+/* eslint-disable sort-keys */
 const reactTest3Props = {
   isStart: true,
   hasData: true,
   existData: true,
   enableSubmit: true,
 } as const
+/* eslint-enable sort-keys */
 
 export const ReactTest4 = () => <ReactTest3 {...reactTest3Props} />
