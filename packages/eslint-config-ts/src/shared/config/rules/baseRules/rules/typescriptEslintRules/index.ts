9852b7840695264f4c18bf550acdc75d412140c6
akky-xxxx <114.s.aki@gmail.com>
2024-06-16 00:47:16 +0900

    feat(eslint-config-ts): add rules
    - consistent-type-exports
    - max-params
    - naming-convention
    - no-import-type-side-effects
    - prefer-destructuring
    - prefer-find

--- packages/eslint-config-ts/src/shared/config/rules/baseRules/rules/typescriptEslintRules/index.ts
+++ packages/eslint-config-ts/src/shared/config/rules/baseRules/rules/typescriptEslintRules/index.ts
@@ -4,6 +4,9 @@ import { consistentTypeImports } from "./options/consistentTypeImports"
 import { noConfusingVoidExpression } from "./options/noConfusingVoidExpression"
 import { noUnnecessaryBooleanLiteralCompare } from "./options/noUnnecessaryBooleanLiteralCompare"
 import { noUnnecessaryCondition } from "./options/noUnnecessaryCondition"
+import { preferDestructuring } from "./options/preferDestructuring"
+import { requireArraySortCompare } from "./options/requireArraySortCompare"
+import { switchExhaustivenessCheck } from "./options/switchExhaustivenessCheck"
 import { SEVERITY } from "../../../../../../libs/shared-for-config/constants/SEVERITY"
 
 import type { EslintRules } from "../../../../../../libs/shared-for-config/types/EslintRules"
@@ -14,11 +17,15 @@ export const typescriptEslintRules = {
   "@typescript-eslint/consistent-indexed-object-style": [SEVERITY.ERROR, "record"],
   "@typescript-eslint/consistent-type-assertions": consistentTypeAssertions,
   "@typescript-eslint/consistent-type-definitions": [SEVERITY.ERROR, "type"],
+  "@typescript-eslint/consistent-type-exports": SEVERITY.ERROR,
   "@typescript-eslint/consistent-type-imports": consistentTypeImports,
   "@typescript-eslint/explicit-module-boundary-types": SEVERITY.OFF,
+  "@typescript-eslint/max-params": SEVERITY.WARN,
+  "@typescript-eslint/naming-convention": SEVERITY.ERROR,
   "@typescript-eslint/no-confusing-void-expression": noConfusingVoidExpression,
   "@typescript-eslint/no-empty-function": SEVERITY.ERROR,
   "@typescript-eslint/no-empty-interface": SEVERITY.WARN,
+  "@typescript-eslint/no-import-type-side-effects": SEVERITY.ERROR,
   "@typescript-eslint/no-inferrable-types": SEVERITY.WARN,
   "@typescript-eslint/no-magic-numbers": SEVERITY.WARN,
   "@typescript-eslint/no-misused-promises": SEVERITY.WARN,
@@ -29,7 +36,12 @@ export const typescriptEslintRules = {
   "@typescript-eslint/no-unnecessary-type-arguments": SEVERITY.WARN,
   "@typescript-eslint/no-unused-vars": SEVERITY.ERROR,
   "@typescript-eslint/no-use-before-define": SEVERITY.ERROR,
+  "@typescript-eslint/prefer-destructuring": preferDestructuring,
+  "@typescript-eslint/prefer-find": SEVERITY.ERROR,
   "@typescript-eslint/prefer-namespace-keyword": SEVERITY.ERROR,
   "@typescript-eslint/prefer-string-starts-ends-with": SEVERITY.WARN,
+  "@typescript-eslint/require-array-sort-compare": requireArraySortCompare,
   "@typescript-eslint/sort-type-constituents": SEVERITY.ERROR,
+  "@typescript-eslint/strict-boolean-expressions": SEVERITY.ERROR,
+  "@typescript-eslint/switch-exhaustiveness-check": switchExhaustivenessCheck,
 } as const satisfies EslintRules
