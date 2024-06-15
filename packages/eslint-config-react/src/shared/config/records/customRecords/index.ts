bed2b6b8b204f5c424161f729731bbe3219c1a6a
akky-xxxx <114.s.aki@gmail.com>
2024-06-15 16:45:40 +0900

    feat(eslint-config-react): extend eslint-config-sc-js.customRecords

--- packages/eslint-config-react/src/shared/config/records/customRecords/index.ts
+++ packages/eslint-config-react/src/shared/config/records/customRecords/index.ts
@@ -1,8 +1,11 @@
+import eslintConfigSCJs from "eslint-config-sc-js"
+
 import { baseRules } from "../../rules/baseRules"
 
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
 export const customRecords = [
+  ...eslintConfigSCJs.configs.customRecords,
   {
     rules: {
       ...baseRules,
