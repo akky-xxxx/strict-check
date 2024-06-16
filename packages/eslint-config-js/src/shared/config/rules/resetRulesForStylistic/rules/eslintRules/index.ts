eff01d9be4d3a96d24f92f473920215dad79d308
akky-xxxx <114.s.aki@gmail.com>
2024-06-16 15:27:53 +0900

    refactor(eslint-config-js): turn comma-dangle to off
    checked by stylistic

--- packages/eslint-config-js/src/shared/config/rules/resetRulesForStylistic/rules/eslintRules/index.ts
+++ packages/eslint-config-js/src/shared/config/rules/resetRulesForStylistic/rules/eslintRules/index.ts
@@ -3,6 +3,7 @@ import { SEVERITY } from "../../../../../../libs/shared-for-config/constants/SEV
 import type { EslintRules } from "../../../../../../libs/shared-for-config/types/EslintRules"
 
 export const eslintRules = {
+  "comma-dangle": SEVERITY.OFF,
   "max-len": SEVERITY.OFF,
   "no-multi-spaces": SEVERITY.OFF,
   "object-curly-newline": SEVERITY.OFF,
