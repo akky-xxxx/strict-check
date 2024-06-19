65e0551cacd60c13d38b22258ac490f8c7fa1334
akky-xxxx <114.s.aki@gmail.com>
2024-06-20 00:13:27 +0900

    refactor(eslint-config-js): separate customRecords to individual records

--- packages/eslint-config-js/src/shared/config/records/customRecords/index.ts
+++ packages/eslint-config-js/src/shared/config/records/customRecords/index.ts
@@ -1,11 +1,7 @@
-import { baseRules } from "../../rules/baseRules"
+import { customRecord } from "../customRecord"
 
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
 export const customRecords = [
-  {
-    rules: {
-      ...baseRules,
-    },
-  },
+  customRecord,
 ] as const satisfies EslintFlatConfig[]
