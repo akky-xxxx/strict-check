88c49d982393cd455d3c446b49f1b6f18f818dc4
akky-xxxx <114.s.aki@gmail.com>
2024-06-20 23:14:09 +0900

    refactor(eslint-config-react): add todo comments

--- packages/eslint-config-react/src/shared/config/records/baseRecords1/index.ts
+++ packages/eslint-config-react/src/shared/config/records/baseRecords1/index.ts
@@ -2,6 +2,7 @@ import eslintConfigSCJs from "eslint-config-sc-js"
 
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
+// TODO: 0.0.3 以降で削除
 export const baseRecords1 = [
   ...eslintConfigSCJs.configs.baseRecords1,
 ] as const satisfies EslintFlatConfig[]
--- packages/eslint-config-react/src/shared/config/records/customRecordsWithTypescript/index.ts
+++ packages/eslint-config-react/src/shared/config/records/customRecordsWithTypescript/index.ts
@@ -2,6 +2,7 @@ import { typescriptRules } from "../../rules/typescriptRules"
 
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
+// TODO: 0.0.3 以降で削除
 export const customRecordsWithTypescript = [
   {
     files: ["**/*.ts", "**/*.tsx"],
