efd80dcf70f9c8f6eca93fdd4cba4ecd325dc32b
akky-xxxx <114.s.aki@gmail.com>
2024-06-20 23:54:34 +0900

    feat(eslint-config-storybook): define separated records

---
+++ packages/eslint-config-storybook/src/shared/config/records/storybookConfigRecords/index.ts
@@ -0,0 +1,16 @@
+import { getCompatExtends } from "../../../../libs/shared-for-config/utilities/getCompatExtends"
+import { FILES } from "../../../const/FILES"
+
+import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
+
+export const storybookConfigRecords = [
+  {
+    files: FILES.BASE,
+  },
+  ...getCompatExtends(
+    "plugin:storybook/addon-interactions",
+    "plugin:storybook/csf",
+    "plugin:storybook/csf-strict",
+    "plugin:storybook/recommended",
+  ),
+] as const satisfies EslintFlatConfig[]
