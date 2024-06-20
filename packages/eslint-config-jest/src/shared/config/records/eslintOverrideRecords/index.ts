65edc6d5a0853920d44f980d00df5cdfc78d9eec
akky-xxxx <114.s.aki@gmail.com>
2024-06-20 23:48:22 +0900

    refactor(eslint-config-jest): add todo comments

--- packages/eslint-config-jest/src/shared/config/records/eslintOverrideRecords/index.ts
+++ packages/eslint-config-jest/src/shared/config/records/eslintOverrideRecords/index.ts
@@ -3,6 +3,7 @@ import { eslintOverrideRules } from "../../rules/eslintOverrideRules"
 
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
+// TODO: 0.0.4 以降で削除
 export const eslintOverrideRecords = [
   {
     files: FILES.JS,
