b32ea11e51df1dad5907fc9724bda398e73c1ac7
akky-xxxx <114.s.aki@gmail.com>
2024-06-22 00:08:07 +0900

    feat(eslint-config-ts): include separated records

--- packages/eslint-config-ts/src/index.ts
+++ packages/eslint-config-ts/src/index.ts
@@ -1,9 +1,23 @@
 import { flatConfig } from "./flatConfig"
 import { legacyConfig } from "./legacyConfig"
+import { airbnbBaseRecords } from "./shared/config/records/airbnbBaseRecords"
 import { baseRecords1 } from "./shared/config/records/baseRecords1"
 import { baseRecords2 } from "./shared/config/records/baseRecords2"
+import { customRecord } from "./shared/config/records/customRecord"
 import { customRecords } from "./shared/config/records/customRecords"
+import { eslintRecommendedRecord } from "./shared/config/records/eslintRecommendedRecord"
+import { initialRecord } from "./shared/config/records/initialRecord"
+import { resetRecordForStylistic } from "./shared/config/records/resetRecordForStylistic"
 import { resetRecordsForStylistic } from "./shared/config/records/resetRecordsForStylistic"
+import { scJsCustomRecord } from "./shared/config/records/scJsCustomRecord"
+import { stylisticRecord } from "./shared/config/records/stylisticRecord"
+import {
+  typescriptEslintStrictTypeCheckedRecords,
+} from "./shared/config/records/typescriptEslintStrictTypeCheckedRecords"
+import {
+  typescriptEslintStylisticTypeCheckedRecords,
+} from "./shared/config/records/typescriptEslintStylisticTypeCheckedRecords"
+import { unicornRecommendedRecords } from "./shared/config/records/unicornRecommendedRecords"
 
 import type { Plugin } from "./libs/shared-for-config/types/EslintPlugin"
 
@@ -16,11 +30,25 @@ const plugin = {
     legacy: legacyConfig,
     recommended: flatConfig,
 
+    // TODO: 0.0.4 以降で削除 ここから
     baseRecords1,
     baseRecords2,
 
     customRecords,
     resetRecordsForStylistic,
+    // TODO: 0.0.4 以降で削除 ここまで
+
+    airbnbBaseRecords,
+    eslintRecommendedRecord,
+    initialRecord,
+    resetRecordForStylistic,
+    scJsCustomRecord,
+    stylisticRecord,
+    typescriptEslintStrictTypeCheckedRecords,
+    typescriptEslintStylisticTypeCheckedRecords,
+    unicornRecommendedRecords,
+
+    customRecord,
   },
 } as const satisfies Plugin
 
