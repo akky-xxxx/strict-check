43d1fff17e1a356657f4b3c934f264e45573d125
akky-xxxx <114.s.aki@gmail.com>
2024-06-22 16:27:00 +0900

    feat(eslint-config-js): add name property

--- packages/eslint-config-js/src/shared/config/records/eslintRecommendedRecord/index.ts
+++ packages/eslint-config-js/src/shared/config/records/eslintRecommendedRecord/index.ts
@@ -1,7 +1,10 @@
 import eslint from "@eslint/js"
 
+import { PACKAGE_NAME } from "../../../constants/PACKAGE_NAME"
+
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
 export const eslintRecommendedRecord = {
   ...eslint.configs.recommended,
+  name: `${PACKAGE_NAME}/eslintRecommendedRecord`,
 } as const satisfies EslintFlatConfig
--- packages/eslint-config-js/src/shared/config/records/initialRecord/index.ts
+++ packages/eslint-config-js/src/shared/config/records/initialRecord/index.ts
@@ -1,8 +1,11 @@
 import { SEVERITY } from "../../../../libs/shared-for-config/constants/SEVERITY"
+import { PACKAGE_NAME } from "../../../constants/PACKAGE_NAME"
 
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
 export const initialRecord = {
+  name: `${PACKAGE_NAME}/initialRecord`,
+
   linterOptions: {
     reportUnusedDisableDirectives: SEVERITY.ERROR,
   },
