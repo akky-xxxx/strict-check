2e62348e5fc6f9f894e760c115f13877d50c368a
akky-xxxx <114.s.aki@gmail.com>
2024-06-22 16:32:30 +0900

    feat(eslint-config-ts): add name property

--- packages/eslint-config-ts/src/shared/config/records/scJsCustomRecord/index.ts
+++ packages/eslint-config-ts/src/shared/config/records/scJsCustomRecord/index.ts
@@ -1,7 +1,10 @@
 import eslintConfigSCJs from "eslint-config-sc-js"
 
+import { PACKAGE_NAME } from "../../../constants/PACKAGE_NAME"
+
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
 export const scJsCustomRecord = {
   ...eslintConfigSCJs.configs.customRecord,
+  name: `${PACKAGE_NAME}/scJsCustomRecord`,
 } as const satisfies EslintFlatConfig
