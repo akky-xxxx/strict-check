0ddd4142160fbcffe118137ef549db0cce947a03
akky-xxxx <114.s.aki@gmail.com>
2024-06-22 16:40:45 +0900

    feat(eslint-config-jest): add name property

--- packages/eslint-config-jest/src/shared/config/records/eslintOverrideRecord/index.ts
+++ packages/eslint-config-jest/src/shared/config/records/eslintOverrideRecord/index.ts
@@ -1,9 +1,12 @@
 import { FILES } from "../../../const/FILES"
+import { PACKAGE_NAME } from "../../../const/PACKAGE_NAME"
 import { eslintOverrideRules } from "../../rules/eslintOverrideRules"
 
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
 export const eslintOverrideRecord = {
+  name: `${PACKAGE_NAME}/eslintOverrideRecord`,
+
   files: FILES.JS,
   rules: {
     ...eslintOverrideRules,
