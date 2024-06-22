eb142aa3fd90b968aa85efe1881fb28163acce55
akky-xxxx <114.s.aki@gmail.com>
2024-06-22 16:48:47 +0900

    feat(eslint-config-next): add name property

--- packages/eslint-config-next/src/shared/config/records/initialRecord/index.ts
+++ packages/eslint-config-next/src/shared/config/records/initialRecord/index.ts
@@ -1,7 +1,10 @@
 import eslintConfigSCReact from "eslint-config-sc-react"
 
+import { PACKAGE_NAME } from "../../../constants/PACKAGE_NAME"
+
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
 export const initialRecord = {
   ...eslintConfigSCReact.configs.initialRecord,
+  name: `${PACKAGE_NAME}/initialRecord`,
 } as const satisfies EslintFlatConfig
--- packages/eslint-config-next/src/shared/config/records/resetRecordForStylistic/index.ts
+++ packages/eslint-config-next/src/shared/config/records/resetRecordForStylistic/index.ts
@@ -1,7 +1,10 @@
 import eslintConfigSCReact from "eslint-config-sc-react"
 
+import { PACKAGE_NAME } from "../../../constants/PACKAGE_NAME"
+
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
 export const resetRecordForStylistic = {
   ...eslintConfigSCReact.configs.resetRecordForStylistic,
+  name: `${PACKAGE_NAME}/resetRecordForStylistic`,
 } as const satisfies EslintFlatConfig
