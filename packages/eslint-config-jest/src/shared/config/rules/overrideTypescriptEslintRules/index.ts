995e7f4751b5461272f2a5ee29de329a33cd4c1d
akky-xxxx <114.s.aki@gmail.com>
2024-06-23 20:19:16 +0900

    refactor(eslint-config-jest): remove eslintRules from overrideTypescriptEslintRules

--- packages/eslint-config-jest/src/shared/config/rules/overrideTypescriptEslintRules/index.ts
+++ packages/eslint-config-jest/src/shared/config/rules/overrideTypescriptEslintRules/index.ts
@@ -1,9 +1,7 @@
 import { typescriptEslintRules } from "./rules/typescriptEslintRules"
-import { eslintRules } from "../overrideEslintRules/rules/eslintRules"
 
 import type { EslintRules } from "../../../../libs/shared-for-config/types/EslintRules"
 
 export const overrideTypescriptEslintRules = {
-  ...eslintRules,
   ...typescriptEslintRules,
 } as const satisfies EslintRules
