cbb0b37d81aafd904edc611a139c84b2415193d2
akky-xxxx <114.s.aki@gmail.com>
2024-06-23 20:59:22 +0900

    test(dr-eslint-config-sc-all): adjust test cases for updated eslint-config-storybook

--- packages/eslint-config-all/spec/tests/getConfigs/modules/getConfigsBase/modules/getConfigsBaseTypescript/index.test.ts
+++ packages/eslint-config-all/spec/tests/getConfigs/modules/getConfigsBase/modules/getConfigsBaseTypescript/index.test.ts
@@ -72,7 +72,7 @@ describe("getConfigsBaseForTypescript", () => {
       [12, "eslint-config-sc-react/customRecord"],
       [13, "eslint-config-sc-react/customRecordWithTypescript"],
       [14, "eslint-config-sc-storybook/storybookConfigRecords"],
-      [15, "eslint-config-sc-storybook/typescriptEslintOverrideRecord"],
+      [15, "eslint-config-sc-storybook/overrideTypescriptRecord"],
       [16, "eslint-config-sc-ts/resetRecordForStylistic"],
     ] as const)("%i番目の config の name は「%s」", (index, name) => {
       expect(getConfigName(result[index])).toBe(name)
@@ -99,7 +99,7 @@ describe("getConfigsBaseForTypescript", () => {
       [12, "eslint-config-sc-react/customRecord"],
       [13, "eslint-config-sc-react/customRecordWithTypescript"],
       [14, "eslint-config-sc-storybook/storybookConfigRecords"],
-      [15, "eslint-config-sc-storybook/typescriptEslintOverrideRecord"],
+      [15, "eslint-config-sc-storybook/overrideTypescriptRecord"],
       [16, "eslint-config-sc-jest/jestPluginRecords"],
       [17, "eslint-config-sc-jest/customRecord"],
       [18, "eslint-config-sc-jest/overrideTypescriptRecord"],
