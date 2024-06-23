87fb633de32f0a1c644334ac10648dc911605b8b
akky-xxxx <114.s.aki@gmail.com>
2024-06-23 18:11:34 +0900

    refactor(eslint-config-storybook): remove eslintRules from typescriptEslintOverrideRules

--- packages/eslint-config-storybook/src/shared/config/rules/typescriptEslintOverrideRules/index.ts
+++ packages/eslint-config-storybook/src/shared/config/rules/typescriptEslintOverrideRules/index.ts
@@ -1,9 +1,7 @@
 import { typescriptEslintRules } from "./rules/typescriptEslintRules"
-import { eslintRules } from "../eslintOverrideRules/rules/eslintRules"
 
 import type { EslintRules } from "../../../../libs/shared-for-config/types/EslintRules"
 
 export const typescriptEslintOverrideRules = {
-  ...eslintRules,
   ...typescriptEslintRules,
 } as const satisfies EslintRules
