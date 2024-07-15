5c01165dd09ba74665f0732acba3c8d7e003b745
akky-xxxx <114.s.aki@gmail.com>
2024-07-15 15:00:15 +0900

    feat(eslint-config-ts): turn off the no-unused-expressions

--- packages/eslint-config-ts/src/shared/config/rules/baseRules/rules/eslintRules/index.ts
+++ packages/eslint-config-ts/src/shared/config/rules/baseRules/rules/eslintRules/index.ts
@@ -9,6 +9,7 @@ export const eslintRules = {
   "no-magic-numbers": SEVERITY.OFF,
   "no-multiple-empty-lines": SEVERITY.OFF,
   "no-shadow": SEVERITY.OFF,
+  "no-unused-expressions": SEVERITY.OFF,
   "no-unused-vars": SEVERITY.OFF,
   "no-use-before-define": SEVERITY.OFF,
   "prefer-destructuring": SEVERITY.OFF,
