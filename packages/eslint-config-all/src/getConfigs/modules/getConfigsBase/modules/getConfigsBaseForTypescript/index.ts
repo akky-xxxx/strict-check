15bec85d6c2a41e866a67374747124325cb6c7fb
akky-xxxx <114.s.aki@gmail.com>
2024-07-15 17:45:54 +0900

    fix(eslint-config-all): switch config order

--- packages/eslint-config-all/src/getConfigs/modules/getConfigsBase/modules/getConfigsBaseForTypescript/index.ts
+++ packages/eslint-config-all/src/getConfigs/modules/getConfigsBase/modules/getConfigsBaseForTypescript/index.ts
@@ -30,8 +30,8 @@ export const getConfigsBaseForTypescript: GetConfigsBaseForTypescript = (librari
 
     const reactConfig = require("eslint-config-sc-react")
     configBase.push(
-      reactConfig.configs.reactRecords,
       reactConfig.configs.airbnbRecords,
+      reactConfig.configs.reactRecords,
       reactConfig.configs.scJsCustomRecord,
       ...tsCommonRecords,
       reactConfig.configs.customRecord,
