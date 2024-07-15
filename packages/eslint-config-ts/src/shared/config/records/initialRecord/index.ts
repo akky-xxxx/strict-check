157168d0d652e61eabe1786e151f3c67f4266bdf
akky-xxxx <114.s.aki@gmail.com>
2024-07-15 18:00:55 +0900

    fix(eslint-config-ts): include settings into initialRecord

--- packages/eslint-config-ts/src/shared/config/records/initialRecord/index.ts
+++ packages/eslint-config-ts/src/shared/config/records/initialRecord/index.ts
@@ -1,10 +1,12 @@
 import eslintConfigSCJs from "eslint-config-sc-js"
 
 import { PACKAGE_NAME } from "../../../constants/PACKAGE_NAME"
+import { settings } from "../../settings"
 
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
 export const initialRecord = {
   ...eslintConfigSCJs.configs.initialRecord,
   name: `${PACKAGE_NAME}/initialRecord`,
+  settings,
 } as const satisfies EslintFlatConfig
