326cdeef573b3c2674b4252a3d0b68a010263659
akky-xxxx <114.s.aki@gmail.com>
2025-07-09 00:02:19 +0900

    chore(*): uninstall eslint-config-airbnb, eslint-config-airbnb-base

--- config/eslint/records/BASE_RECORDS/index.mjs
+++ config/eslint/records/BASE_RECORDS/index.mjs
@@ -1,18 +1,10 @@
-import { FlatCompat } from "@eslint/eslintrc"
 import eslint from "@eslint/js"
 import stylistic from "@stylistic/eslint-plugin"
-import path from "node:path"
-import { fileURLToPath } from "node:url"
 
 import { BASE_RULES } from "../../rules/BASE_RULES/index.mjs"
 
-const compat = new FlatCompat({
-  baseDirectory: path.dirname(fileURLToPath(import.meta.url)),
-})
-
 export const BASE_RECORDS = [
   eslint.configs.recommended,
-  ...compat.extends("eslint-config-airbnb-base"),
   stylistic.configs["recommended-flat"],
   {
     ignores: [
