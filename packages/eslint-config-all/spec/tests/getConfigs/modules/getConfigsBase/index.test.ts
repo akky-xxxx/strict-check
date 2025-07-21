d1856fa02cb268fbe5ee76961efbfc9d0a4d03c5
akky-xxxx <114.s.aki@gmail.com>
2025-07-21 17:49:36 +0900

    feat(eslint-config-all): use importRecommendedRecord

--- packages/eslint-config-all/spec/tests/getConfigs/modules/getConfigsBase/index.test.ts
+++ packages/eslint-config-all/spec/tests/getConfigs/modules/getConfigsBase/index.test.ts
@@ -4,84 +4,88 @@ import { getConfigsBase } from "../../../../../src/getConfigs/modules/getConfigs
 describe("getConfigsBase", () => {
   describe("javascript", () => {
     const result = getConfigsBase("javascript")
-    it("getConfigsBase の戻り値は配列長7", () => {
-      expect(result).toHaveLength(7)
+    it("getConfigsBase の戻り値は配列長8", () => {
+      expect(result).toHaveLength(8)
     })
     it.each([
       [0, "eslint-config-sc-js/initialRecord"],
-      [1, "eslint-config-sc-js/stylisticRecord"],
-      [2, "eslint-config-sc-js/eslintRecommendedRecord"],
-      [3, "eslint-config-sc-js/unicornRecommendedRecords"],
-      [4, "eslint-config-sc-js/airbnbBaseRecords"],
-      [5, "eslint-config-sc-js/customRecord"],
-      [6, "eslint-config-sc-js/resetRecordForStylistic"],
+      [1, "eslint-config-sc-js/importRecommendedRecord"],
+      [2, "eslint-config-sc-js/stylisticRecord"],
+      [3, "eslint-config-sc-js/eslintRecommendedRecord"],
+      [4, "eslint-config-sc-js/unicornRecommendedRecords"],
+      [5, "eslint-config-sc-js/airbnbBaseRecords"],
+      [6, "eslint-config-sc-js/customRecord"],
+      [7, "eslint-config-sc-js/resetRecordForStylistic"],
     ] as const)("%i番目の config の name は「%s」", (index, name) => {
       expect(getConfigName(result[index])).toBe(name)
     })
   })
   describe("javascript with jest, storybook", () => {
     const result = getConfigsBase("javascript", ["jest", "storybook"])
-    it("getConfigsBase の戻り値は配列長12", () => {
-      expect(result).toHaveLength(12)
+    it("getConfigsBase の戻り値は配列長13", () => {
+      expect(result).toHaveLength(13)
     })
     it.each([
       [0, "eslint-config-sc-js/initialRecord"],
-      [1, "eslint-config-sc-js/stylisticRecord"],
-      [2, "eslint-config-sc-js/eslintRecommendedRecord"],
-      [3, "eslint-config-sc-js/unicornRecommendedRecords"],
-      [4, "eslint-config-sc-js/airbnbBaseRecords"],
-      [5, "eslint-config-sc-js/customRecord"],
-      [6, "eslint-config-sc-storybook/storybookConfigRecords"],
-      [7, "eslint-config-sc-storybook/overrideJavascriptRecord"],
-      [8, "eslint-config-sc-jest/jestPluginRecords"],
-      [9, "eslint-config-sc-jest/customRecord"],
-      [10, "eslint-config-sc-jest/overrideJavascriptRecord"],
-      [11, "eslint-config-sc-js/resetRecordForStylistic"],
+      [1, "eslint-config-sc-js/importRecommendedRecord"],
+      [2, "eslint-config-sc-js/stylisticRecord"],
+      [3, "eslint-config-sc-js/eslintRecommendedRecord"],
+      [4, "eslint-config-sc-js/unicornRecommendedRecords"],
+      [5, "eslint-config-sc-js/airbnbBaseRecords"],
+      [6, "eslint-config-sc-js/customRecord"],
+      [7, "eslint-config-sc-storybook/storybookConfigRecords"],
+      [8, "eslint-config-sc-storybook/overrideJavascriptRecord"],
+      [9, "eslint-config-sc-jest/jestPluginRecords"],
+      [10, "eslint-config-sc-jest/customRecord"],
+      [11, "eslint-config-sc-jest/overrideJavascriptRecord"],
+      [12, "eslint-config-sc-js/resetRecordForStylistic"],
     ] as const)("%i番目の config の name は「%s」", (index, name) => {
       expect(getConfigName(result[index])).toBe(name)
     })
   })
   describe("typescript", () => {
     const result = getConfigsBase("typescript")
-    it("getConfigsBase の戻り値は配列長10", () => {
-      expect(result).toHaveLength(10)
+    it("getConfigsBase の戻り値は配列長11", () => {
+      expect(result).toHaveLength(11)
     })
     it.each([
       [0, "eslint-config-sc-ts/initialRecord"],
-      [1, "eslint-config-sc-ts/stylisticRecord"],
-      [2, "eslint-config-sc-ts/eslintRecommendedRecord"],
-      [3, "eslint-config-sc-ts/unicornRecommendedRecords"],
-      [4, "eslint-config-sc-ts/typescriptEslintStrictTypeCheckedRecords"],
-      [5, "eslint-config-sc-ts/typescriptEslintStylisticTypeCheckedRecords"],
-      [6, "eslint-config-sc-ts/airbnbBaseRecords"],
-      [7, "eslint-config-sc-ts/scJsCustomRecord"],
-      [8, "eslint-config-sc-ts/customRecord"],
-      [9, "eslint-config-sc-ts/resetRecordForStylistic"],
+      [1, "eslint-config-sc-ts/importRecommendedRecord"],
+      [2, "eslint-config-sc-ts/stylisticRecord"],
+      [3, "eslint-config-sc-ts/eslintRecommendedRecord"],
+      [4, "eslint-config-sc-ts/unicornRecommendedRecords"],
+      [5, "eslint-config-sc-ts/typescriptEslintStrictTypeCheckedRecords"],
+      [6, "eslint-config-sc-ts/typescriptEslintStylisticTypeCheckedRecords"],
+      [7, "eslint-config-sc-ts/airbnbBaseRecords"],
+      [8, "eslint-config-sc-ts/scJsCustomRecord"],
+      [9, "eslint-config-sc-ts/customRecord"],
+      [10, "eslint-config-sc-ts/resetRecordForStylistic"],
     ] as const)("%i番目の config の name は「%s」", (index, name) => {
       expect(getConfigName(result[index])).toBe(name)
     })
   })
   describe("typescript with jest, storybook", () => {
     const result = getConfigsBase("typescript", ["jest", "storybook"])
-    it("getConfigsBase の戻り値は配列長15", () => {
-      expect(result).toHaveLength(15)
+    it("getConfigsBase の戻り値は配列長16", () => {
+      expect(result).toHaveLength(16)
     })
     it.each([
       [0, "eslint-config-sc-ts/initialRecord"],
-      [1, "eslint-config-sc-ts/stylisticRecord"],
-      [2, "eslint-config-sc-ts/eslintRecommendedRecord"],
-      [3, "eslint-config-sc-ts/unicornRecommendedRecords"],
-      [4, "eslint-config-sc-ts/typescriptEslintStrictTypeCheckedRecords"],
-      [5, "eslint-config-sc-ts/typescriptEslintStylisticTypeCheckedRecords"],
-      [6, "eslint-config-sc-ts/airbnbBaseRecords"],
-      [7, "eslint-config-sc-ts/scJsCustomRecord"],
-      [8, "eslint-config-sc-ts/customRecord"],
-      [9, "eslint-config-sc-storybook/storybookConfigRecords"],
-      [10, "eslint-config-sc-storybook/overrideTypescriptRecord"],
-      [11, "eslint-config-sc-jest/jestPluginRecords"],
-      [12, "eslint-config-sc-jest/customRecord"],
-      [13, "eslint-config-sc-jest/overrideTypescriptRecord"],
-      [14, "eslint-config-sc-ts/resetRecordForStylistic"],
+      [1, "eslint-config-sc-ts/importRecommendedRecord"],
+      [2, "eslint-config-sc-ts/stylisticRecord"],
+      [3, "eslint-config-sc-ts/eslintRecommendedRecord"],
+      [4, "eslint-config-sc-ts/unicornRecommendedRecords"],
+      [5, "eslint-config-sc-ts/typescriptEslintStrictTypeCheckedRecords"],
+      [6, "eslint-config-sc-ts/typescriptEslintStylisticTypeCheckedRecords"],
+      [7, "eslint-config-sc-ts/airbnbBaseRecords"],
+      [8, "eslint-config-sc-ts/scJsCustomRecord"],
+      [9, "eslint-config-sc-ts/customRecord"],
+      [10, "eslint-config-sc-storybook/storybookConfigRecords"],
+      [11, "eslint-config-sc-storybook/overrideTypescriptRecord"],
+      [12, "eslint-config-sc-jest/jestPluginRecords"],
+      [13, "eslint-config-sc-jest/customRecord"],
+      [14, "eslint-config-sc-jest/overrideTypescriptRecord"],
+      [15, "eslint-config-sc-ts/resetRecordForStylistic"],
     ] as const)("%i番目の config の name は「%s」", (index, name) => {
       expect(getConfigName(result[index])).toBe(name)
     })
--- packages/eslint-config-all/spec/tests/getConfigs/modules/getConfigsBase/modules/getConfigsBaseForJavascript/index.test.ts
+++ packages/eslint-config-all/spec/tests/getConfigs/modules/getConfigsBase/modules/getConfigsBaseForJavascript/index.test.ts
@@ -4,36 +4,15 @@ import { getConfigName } from "../getConfigName"
 describe("getConfigsBaseForJavascript", () => {
   describe("javascript with react", () => {
     const result = getConfigsBaseForJavascript(["react"])
-    it("getConfigsBaseForJavascript の戻り値は配列長10", () => {
-      expect(result).toHaveLength(11)
-    })
-    it.each([
-      [0, "eslint-config-sc-js/initialRecord"],
-      [1, "eslint-config-sc-js/stylisticRecord"],
-      [2, "eslint-config-sc-js/eslintRecommendedRecord"],
-      [3, "eslint-config-sc-js/unicornRecommendedRecords"],
-      [4, "eslint-config-sc-react/initialRecord"],
-      [5, "eslint-config-sc-react/airbnbRecords"],
-      [6, "eslint-config-sc-react/reactRecords"],
-      [7, "eslint-config-sc-react/scJsCustomRecord"],
-      [8, "eslint-config-sc-js/customRecord"],
-      [9, "eslint-config-sc-react/customRecord"],
-      [10, "eslint-config-sc-js/resetRecordForStylistic"],
-    ] as const)("%i番目の config の name は「%s」", (index, name) => {
-      expect(getConfigName(result[index])).toBe(name)
-    })
-  })
-  describe("javascript with react, next", () => {
-    const result = getConfigsBaseForJavascript(["next", "react"])
-    it("getConfigsBaseForJavascript の戻り値は配列長11", () => {
+    it("getConfigsBaseForJavascript の戻り値は配列長12", () => {
       expect(result).toHaveLength(12)
     })
     it.each([
       [0, "eslint-config-sc-js/initialRecord"],
-      [1, "eslint-config-sc-js/stylisticRecord"],
-      [2, "eslint-config-sc-js/eslintRecommendedRecord"],
-      [3, "eslint-config-sc-js/unicornRecommendedRecords"],
-      [4, "eslint-config-sc-next/nextRecord"],
+      [1, "eslint-config-sc-js/importRecommendedRecord"],
+      [2, "eslint-config-sc-js/stylisticRecord"],
+      [3, "eslint-config-sc-js/eslintRecommendedRecord"],
+      [4, "eslint-config-sc-js/unicornRecommendedRecords"],
       [5, "eslint-config-sc-react/initialRecord"],
       [6, "eslint-config-sc-react/airbnbRecords"],
       [7, "eslint-config-sc-react/reactRecords"],
@@ -45,53 +24,78 @@ describe("getConfigsBaseForJavascript", () => {
       expect(getConfigName(result[index])).toBe(name)
     })
   })
-  describe("javascript with react, next, storybook", () => {
-    const result = getConfigsBaseForJavascript(["next", "react", "storybook"])
+  describe("javascript with react, next", () => {
+    const result = getConfigsBaseForJavascript(["next", "react"])
     it("getConfigsBaseForJavascript の戻り値は配列長13", () => {
-      expect(result).toHaveLength(14)
+      expect(result).toHaveLength(13)
     })
     it.each([
       [0, "eslint-config-sc-js/initialRecord"],
-      [1, "eslint-config-sc-js/stylisticRecord"],
-      [2, "eslint-config-sc-js/eslintRecommendedRecord"],
-      [3, "eslint-config-sc-js/unicornRecommendedRecords"],
-      [4, "eslint-config-sc-next/nextRecord"],
-      [5, "eslint-config-sc-react/initialRecord"],
-      [6, "eslint-config-sc-react/airbnbRecords"],
-      [7, "eslint-config-sc-react/reactRecords"],
-      [8, "eslint-config-sc-react/scJsCustomRecord"],
-      [9, "eslint-config-sc-js/customRecord"],
-      [10, "eslint-config-sc-react/customRecord"],
-      [11, "eslint-config-sc-storybook/storybookConfigRecords"],
-      [12, "eslint-config-sc-storybook/overrideJavascriptRecord"],
-      [13, "eslint-config-sc-js/resetRecordForStylistic"],
+      [1, "eslint-config-sc-js/importRecommendedRecord"],
+      [2, "eslint-config-sc-js/stylisticRecord"],
+      [3, "eslint-config-sc-js/eslintRecommendedRecord"],
+      [4, "eslint-config-sc-js/unicornRecommendedRecords"],
+      [5, "eslint-config-sc-next/nextRecord"],
+      [6, "eslint-config-sc-react/initialRecord"],
+      [7, "eslint-config-sc-react/airbnbRecords"],
+      [8, "eslint-config-sc-react/reactRecords"],
+      [9, "eslint-config-sc-react/scJsCustomRecord"],
+      [10, "eslint-config-sc-js/customRecord"],
+      [11, "eslint-config-sc-react/customRecord"],
+      [12, "eslint-config-sc-js/resetRecordForStylistic"],
+    ] as const)("%i番目の config の name は「%s」", (index, name) => {
+      expect(getConfigName(result[index])).toBe(name)
+    })
+  })
+  describe("javascript with react, next, storybook", () => {
+    const result = getConfigsBaseForJavascript(["next", "react", "storybook"])
+    it("getConfigsBaseForJavascript の戻り値は配列長15", () => {
+      expect(result).toHaveLength(15)
+    })
+    it.each([
+      [0, "eslint-config-sc-js/initialRecord"],
+      [1, "eslint-config-sc-js/importRecommendedRecord"],
+      [2, "eslint-config-sc-js/stylisticRecord"],
+      [3, "eslint-config-sc-js/eslintRecommendedRecord"],
+      [4, "eslint-config-sc-js/unicornRecommendedRecords"],
+      [5, "eslint-config-sc-next/nextRecord"],
+      [6, "eslint-config-sc-react/initialRecord"],
+      [7, "eslint-config-sc-react/airbnbRecords"],
+      [8, "eslint-config-sc-react/reactRecords"],
+      [9, "eslint-config-sc-react/scJsCustomRecord"],
+      [10, "eslint-config-sc-js/customRecord"],
+      [11, "eslint-config-sc-react/customRecord"],
+      [12, "eslint-config-sc-storybook/storybookConfigRecords"],
+      [13, "eslint-config-sc-storybook/overrideJavascriptRecord"],
+      [14, "eslint-config-sc-js/resetRecordForStylistic"],
     ] as const)("%i番目の config の name は「%s」", (index, name) => {
       expect(getConfigName(result[index])).toBe(name)
     })
   })
   describe("javascript with react, next, storybook, jest", () => {
     const result = getConfigsBaseForJavascript(["next", "react", "storybook", "jest"])
-    it("getConfigsBaseForJavascript の戻り値は配列長16", () => {
-      expect(result).toHaveLength(17)
+    it("getConfigsBaseForJavascript の戻り値は配列長18", () => {
+      expect(result).toHaveLength(18)
     })
     it.each([
       [0, "eslint-config-sc-js/initialRecord"],
-      [1, "eslint-config-sc-js/stylisticRecord"],
-      [2, "eslint-config-sc-js/eslintRecommendedRecord"],
-      [3, "eslint-config-sc-js/unicornRecommendedRecords"],
-      [4, "eslint-config-sc-next/nextRecord"],
-      [5, "eslint-config-sc-react/initialRecord"],
-      [6, "eslint-config-sc-react/airbnbRecords"],
-      [7, "eslint-config-sc-react/reactRecords"],
-      [8, "eslint-config-sc-react/scJsCustomRecord"],
-      [9, "eslint-config-sc-js/customRecord"],
-      [10, "eslint-config-sc-react/customRecord"],
-      [11, "eslint-config-sc-storybook/storybookConfigRecords"],
-      [12, "eslint-config-sc-storybook/overrideJavascriptRecord"],
-      [13, "eslint-config-sc-jest/jestPluginRecords"],
-      [14, "eslint-config-sc-jest/customRecord"],
-      [15, "eslint-config-sc-jest/overrideJavascriptRecord"],
-      [16, "eslint-config-sc-js/resetRecordForStylistic"],
+      [1, "eslint-config-sc-js/importRecommendedRecord"],
+      [2, "eslint-config-sc-js/stylisticRecord"],
+      [3, "eslint-config-sc-js/eslintRecommendedRecord"],
+      [4, "eslint-config-sc-js/unicornRecommendedRecords"],
+      [5, "eslint-config-sc-next/nextRecord"],
+      [6, "eslint-config-sc-react/initialRecord"],
+      [7, "eslint-config-sc-react/airbnbRecords"],
+      [8, "eslint-config-sc-react/reactRecords"],
+      [9, "eslint-config-sc-react/scJsCustomRecord"],
+      [10, "eslint-config-sc-js/customRecord"],
+      [11, "eslint-config-sc-react/customRecord"],
+      [12, "eslint-config-sc-storybook/storybookConfigRecords"],
+      [13, "eslint-config-sc-storybook/overrideJavascriptRecord"],
+      [14, "eslint-config-sc-jest/jestPluginRecords"],
+      [15, "eslint-config-sc-jest/customRecord"],
+      [16, "eslint-config-sc-jest/overrideJavascriptRecord"],
+      [17, "eslint-config-sc-js/resetRecordForStylistic"],
     ] as const)("%i番目の config の name は「%s」", (index, name) => {
       expect(getConfigName(result[index])).toBe(name)
     })
