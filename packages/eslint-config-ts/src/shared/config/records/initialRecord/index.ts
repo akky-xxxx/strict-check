da877b033f587efc49e9cd64ebb93e18cda41bd3
akky-xxxx <114.s.aki@gmail.com>
2026-03-29 14:26:16 +0900

    refactor(eslint-config-ts): keep initialRecord unused-disable policy unchanged

--- packages/eslint-config-ts/src/shared/config/records/initialRecord/index.ts
+++ packages/eslint-config-ts/src/shared/config/records/initialRecord/index.ts
@@ -1,6 +1,5 @@
 import eslintConfigSCJs from "eslint-config-sc-js"
 
-import { SEVERITY } from "../../../../libs/shared-for-config/constants/SEVERITY"
 import { PACKAGE_NAME } from "../../../constants/PACKAGE_NAME"
 import { settings } from "../../settings"
 
@@ -8,10 +7,6 @@ import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/
 
 export const initialRecord = {
   ...eslintConfigSCJs.configs.initialRecord,
-  linterOptions: {
-    ...eslintConfigSCJs.configs.initialRecord.linterOptions,
-    reportUnusedDisableDirectives: SEVERITY.OFF,
-  },
   name: `${PACKAGE_NAME}/initialRecord`,
   settings,
 } as const satisfies EslintFlatConfig
