581b1a75c484ff8cc5a632830f22aec1b3cacdce
akky-xxxx <114.s.aki@gmail.com>
2026-04-05 23:39:37 +0900

    refactor(eslint-config-js): inline reset style rules and remove resetRules module

--- packages/eslint-config-js/src/shared/config/records/resetRecordForStyle/index.ts
+++ packages/eslint-config-js/src/shared/config/records/resetRecordForStyle/index.ts
@@ -1,11 +1,22 @@
+import { SEVERITY } from "../../../../libs/shared-for-config/constants/SEVERITY"
 import { PACKAGE_NAME } from "../../../constants/PACKAGE_NAME"
-import { resetRulesForStyle } from "../../rules/resetRulesForStyle"
 
 import type { EslintFlatConfig } from "../../../../libs/shared-for-config/types/EslintFlatConfig"
 
 export const resetRecordForStyle = {
   name: `${PACKAGE_NAME}/resetRecordForStyle`,
   rules: {
-    ...resetRulesForStyle,
+    "arrow-parens": SEVERITY.OFF,
+    "brace-style": SEVERITY.OFF,
+    "comma-dangle": SEVERITY.OFF,
+    "key-spacing": SEVERITY.OFF,
+    "keyword-spacing": SEVERITY.OFF,
+    "max-len": SEVERITY.OFF,
+    "no-multi-spaces": SEVERITY.OFF,
+    "object-curly-newline": SEVERITY.OFF,
+    "quote-props": SEVERITY.OFF,
+    "quotes": SEVERITY.OFF,
+    "semi": SEVERITY.OFF,
+    "space-before-blocks": SEVERITY.OFF,
   },
 } as const satisfies EslintFlatConfig
